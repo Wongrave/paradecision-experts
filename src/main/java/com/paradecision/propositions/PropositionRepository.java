3a75a0dc2cc16b34c44e748b0af0ef2836c935c9
wongrave <tomiatti@hotmail.com>
2020-02-24 19:05:32 -0300

    devolve proposicao por perfil e organizacao deploy

--- src/main/java/com/paradecision/propositions/PropositionRepository.java
+++ src/main/java/com/paradecision/propositions/PropositionRepository.java
@@ -15,7 +15,7 @@ public interface PropositionRepository extends JpaRepository<Proposition, Long> 
             value = "SELECT DISTINCT p.* " +
                     "FROM proposition_01 p " +
                     "JOIN profile_11 p11 ON p11.a02_id = p.a02_id " +
-                    "WHERE p11.a02_id = :userId AND" +
+                    "WHERE p11.a02_id = :userId AND " +
                     "p.a20_id = :organizationId",
             nativeQuery = true)
     Optional<List<Proposition>> findAllByProfile(Long userId, Long organizationId);
